ac22720e005e4cd54eb2e24350f9ee06e105b47e
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-05-15 21:29:20 +0530

    chore: Refactor UserProfileAbout component and add edit button for current user

--- src/components/Home/UserProfile/UserNavs/UserProfileAbout.jsx
+++ src/components/Home/UserProfile/UserNavs/UserProfileAbout.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import { Blog } from '../../../../contextAPI/Context'
 
 const UserProfileAbout = ({getUsersData, setEditModal}) => {
+  const {currUser} = Blog();
   return (
     <div className='min-h-screen'>
       <p className='text-2xl first-letter:uppercase'>
         {getUsersData?.bio || getUsersData?.username + " has not updated their bio yet"}
       </p>
       <div className='text-right'>
+        {currUser?.uid === getUsersData?.userId && 
         <button 
         onClick={() => setEditModal(true)}
         className='border border-white py-2 px-5 rounded-full bg-header2
         text-balck mt-[5rem]'>Edit here</button>
+      }
       </div>
     </div>
   )
